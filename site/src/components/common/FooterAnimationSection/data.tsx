a6576c596b20d2e4caf85be591dad0580f05f383
Ivan Tavruev <ivan.tavruev@saltpepper.ru>
2023-10-31 12:06:50 +0300

    fix back footer animation section and about

--- site/src/components/common/FooterAnimationSection/data.tsx
+++ site/src/components/common/FooterAnimationSection/data.tsx
@@ -20,7 +20,7 @@ export const getAnimatedItemList: AnimationGetter = (
 			id: "1",
 			ySourceValue: "-30%",
 			yTransformValue: "-30%",
-			// hideLine: true,
+			hideLine: true,
 		},
 		{
 			id: "2",
@@ -31,26 +31,26 @@ export const getAnimatedItemList: AnimationGetter = (
 			id: "3",
 			ySourceValue: "-100%",
 			yTransformValue: "-100%",
-			hideLine: true,
-			// children: text && (
-			//   <ArrowButton
-			//     className={s.arrowButton}
-			//     text={text}
-			//     href={href}
-			//     onClick={onClick}
-			//   />
-			// ),
+			// hideLine: true,
+			children: text && (
+				<ArrowButton
+					className={s.arrowButton}
+					text={text}
+					href={href}
+					onClick={onClick}
+				/>
+			),
 		},
 		{
 			id: "4",
 			ySourceValue: "-120%",
 			yTransformValue: "-75%",
 		},
-		// {
-		//   id: '5',
-		//   ySourceValue: '-42%',
-		//   yTransformValue: '-50%',
-		// },
+		{
+			id: "5",
+			ySourceValue: "-42%",
+			yTransformValue: "-50%",
+		},
 		{
 			id: "6",
 			ySourceValue: "85%",
@@ -78,13 +78,13 @@ export const getAnimatedItemList: AnimationGetter = (
 			yTransformValue: "85%",
 			alignment: ANIMATION_CARD_ALIGNMENT.bottom,
 		},
-		// {
-		//   id: '10',
-		//   ySourceValue: '85%',
-		//   // ySourceValue: prefersReducedMotion ? '90%' : '43%',
-		//   yTransformValue: '85%',
-		//   alignment: ANIMATION_CARD_ALIGNMENT.bottom,
-		// },
+		{
+			id: "10",
+			ySourceValue: "85%",
+			// ySourceValue: prefersReducedMotion ? '90%' : '43%',
+			yTransformValue: "85%",
+			alignment: ANIMATION_CARD_ALIGNMENT.bottom,
+		},
 	];
 };
 export const getAnimatedItemMobileList: AnimationGetter = (
--- site/src/components/pages/About/About.tsx
+++ site/src/components/pages/About/About.tsx
@@ -1,36 +1,51 @@
-import Container from 'components/Container'
+import Container from "components/Container";
 
-import JoinSection, { getJoinSectionProps } from 'components/JoinSection'
-import SideNavigation from 'components/SideNavigation'
-import FooterAnimationSection from 'components/FooterAnimationSection'
-import Hero from './Hero'
-import Partners from './Partners'
-import OurTeam from './OurTeam'
-import Toolchain from './Toolchain'
+import JoinSection, { getJoinSectionProps } from "components/JoinSection";
+import SideNavigation from "components/SideNavigation";
+import FooterAnimationSection from "components/FooterAnimationSection";
+import Hero from "./Hero";
+import Partners from "./Partners";
+import OurTeam from "./OurTeam";
+import Toolchain from "./Toolchain";
 
-import s from './About.module.scss'
-import { aboutPageData } from 'stubs/aboutPageData'
+import s from "./About.module.scss";
+import { aboutPageData } from "stubs/aboutPageData";
+import WhiteRectangleLine from "components/WhiteRectangleLine";
+import { useViewport } from "hooks/useViewport";
 
 type AboutProps = {
-  data: typeof aboutPageData
-}
+	data: typeof aboutPageData;
+};
 
 const About = ({ data }: AboutProps) => {
-  return (
-    <Container className={s.container}>
-      <SideNavigation className={s.sideNavigation} title="About" titleAnimation={false} />
-      <div className={s.root}>
-        <div className={s.content}>
-          <Hero data={data.hero} />
-          <Toolchain data={data.toolchain} />
-          <Partners data={data.partners} />
-          <OurTeam data={data.ourTeam} />
-          <JoinSection {...getJoinSectionProps(data)} />
-        </div>
-      </div>
-      <FooterAnimationSection className={s.footerSection} />
-    </Container>
-  )
-}
+	const { isMobile } = useViewport();
 
-export default About
+	return (
+		<Container className={s.container}>
+			<SideNavigation
+				className={s.sideNavigation}
+				title="About"
+				titleAnimation={false}
+			/>
+			<div className={s.root}>
+				<div className={s.content}>
+					<Hero data={data.hero} />
+					<Toolchain data={data.toolchain} />
+					<Partners data={data.partners} />
+					<OurTeam data={data.ourTeam} />
+					<JoinSection {...getJoinSectionProps(data)} />
+
+					<WhiteRectangleLine
+						marginTop={isMobile ? 60 : 142}
+						data={isMobile ? [0] : [112, 112, 136, 0]}
+					/>
+					<div id="footer_nav" />
+				</div>
+			</div>
+
+			{/* <FooterAnimationSection className={s.footerSection} /> */}
+		</Container>
+	);
+};
+
+export default About;

07748d37518cc293a1dab5aaadb05c383e00df3e
Ivan Tavruev <ivan.tavruev@saltpepper.ru>
2023-11-03 13:45:21 +0300

    add classname to white rectangle line

--- site/src/components/common/WhiteRectangleLine/WhiteRectangleLine.tsx
+++ site/src/components/common/WhiteRectangleLine/WhiteRectangleLine.tsx
@@ -1,16 +1,19 @@
 import WhiteRectangle from 'components/WhiteRectangle'
 import React from 'react'
 
+import cx from 'classnames'
+
 import s from './WhiteRectangleLine.module.scss'
 
 type Props = {
   data: { id: number; margin: number }[]
   marginTop: number
+  className: string
 }
 
-const WhiteRectangleLine = ({ data, marginTop }: Props) => {
+const WhiteRectangleLine = ({ data, marginTop, className }: Props) => {
   return (
-    <div style={{ '--margin-top': marginTop } as React.CSSProperties} className={s.root}>
+    <div style={{ '--margin-top': marginTop } as React.CSSProperties} className={cx(s.root, className)}>
       {data.map((item, index) => (
         <div style={{ '--line-margin': item.margin } as React.CSSProperties} key={item.id} className={s.rectangle}>
           <WhiteRectangle />

6b1eacf8458b97b9e5a9fb4969e51691f1392c15
Ivan Tavruev <ivan.tavruev@saltpepper.ru>
2023-11-03 19:01:57 +0300

    add card to about

--- site/src/components/common/LinkCard/LinkCard.tsx
+++ site/src/components/common/LinkCard/LinkCard.tsx
@@ -1,26 +1,29 @@
-import { memo } from 'react'
-import { InferProps, string } from 'prop-types'
-import cx from 'classnames'
+import { memo } from "react";
+import { InferProps, string } from "prop-types";
+import cx from "classnames";
 
-import Button from 'components/Button'
+import s from "./LinkCard.module.scss";
+import { Card } from "components/Card";
 
-import s from './LinkCard.module.scss'
-import { Card } from 'components/Card'
-
-function LinkCard({ className, title, linkTo, description }: InferProps<typeof LinkCard.propTypes>) {
-  return (
-    <Card className={cx(s.root, className)} href={linkTo} disabled={!linkTo}>
-      <h2 className={s.title}>{title}</h2>
-      <p className={s.description}>{description}</p>
-    </Card>
-  )
+function LinkCard({
+	className,
+	title,
+	linkTo,
+	description,
+}: InferProps<typeof LinkCard.propTypes>) {
+	return (
+		<Card className={cx(s.root, className)} href={linkTo}>
+			<h2 className={s.title}>{title}</h2>
+			<p className={s.description}>{description}</p>
+		</Card>
+	);
 }
 
 LinkCard.propTypes = {
-  className: string,
-  title: string,
-  linkTo: string,
-  description: string,
-}
+	className: string,
+	title: string,
+	linkTo: string.isRequired,
+	description: string,
+};
 
-export default memo(LinkCard)
+export default memo(LinkCard);

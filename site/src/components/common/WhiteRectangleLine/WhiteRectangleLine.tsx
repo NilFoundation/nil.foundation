653928be85b0d7bdda0e84b4130d2afd14194d1c
Ivan Tavruev <ivan.tavruev@saltpepper.ru>
2023-11-03 13:50:19 +0300

    refactoring classname not required

--- site/src/components/common/WhiteRectangleLine/WhiteRectangleLine.tsx
+++ site/src/components/common/WhiteRectangleLine/WhiteRectangleLine.tsx
@@ -8,7 +8,7 @@ import s from './WhiteRectangleLine.module.scss'
 type Props = {
   data: { id: number; margin: number }[]
   marginTop: number
-  className: string
+  className?: string
 }
 
 const WhiteRectangleLine = ({ data, marginTop, className }: Props) => {

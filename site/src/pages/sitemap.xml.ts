2cedc2229401ccdc3f55c4f5c67c9c7e00e1a519
ukorvl <u.korotovskiy@nil.foundation>
2023-12-29 18:28:58 +0400

    minor changes #133

--- site/src/pages/sitemap.xml.ts
+++ site/src/pages/sitemap.xml.ts
@@ -96,8 +96,8 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
           .join('')}
 
            ${otherPaths
-            .map(
-              (el) => `
+             .map(
+               (el) => `
                 <url>
                   <loc>${process.env.NEXT_PUBLIC_BASE_URL}/${el.url}</loc>
                   <lastmod>${new Date().toISOString()}</lastmod>
@@ -105,8 +105,8 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
                   <priority>0.7</priority>
                 </url>
                 `,
-            )
-            .join('')}
+             )
+             .join('')}
     </urlset>
   `
 

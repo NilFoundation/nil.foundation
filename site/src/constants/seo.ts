d196c7942d3d3882042d95c5ef45137e3795e42a
ukorvl <ukorotovskiy@gmail.com>
2024-08-28 01:15:34 +0400

    change twitter icon and devnet link

--- site/src/constants/seo.ts
+++ site/src/constants/seo.ts
@@ -5,5 +5,5 @@ export const seo = {
   url: 'https://nil.foundation',
   image: '/og.jpg',
   theme: '#212121',
-  devnetLink: 'https://nilfoundation.typeform.com/nil-devnet?typeform-source=t.co',
+  devnetLink: 'https://nilfoundation.typeform.com/to/AWzcCYlo',
 }

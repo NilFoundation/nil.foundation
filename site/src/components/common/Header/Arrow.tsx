766327a11734d3c188624bc4a8ca784ca89052dd
klond90 <newbiecraft@gmail.com>
2025-02-04 15:56:06 +0100

    add legion banner to header

---
+++ site/src/components/common/Header/Arrow.tsx
@@ -0,0 +1,7 @@
+export const Arrow = ({className}: {className?: string}) => {
+    return <svg className={className} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="2.66797" y="7.3335" width="10.3111" height="1.33333" fill="#BDBDBD"/>
+<rect x="9.34375" y="3.3335" width="6.58798" height="1.33333" transform="rotate(45 9.34375 3.3335)" fill="#BDBDBD"/>
+<rect x="8.40234" y="11.7236" width="6.58588" height="1.33333" transform="rotate(-45 8.40234 11.7236)" fill="#BDBDBD"/>
+</svg>
+}

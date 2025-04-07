de2540e9552f7518eff764061fe9b5c0b755e102
klond90 <newbiecraft@gmail.com>
2025-04-07 18:44:34 +0700

    remove uniswap widget untill it will be fixed on api part

--- site/src/components/pages/LandingPage/LandingPage.tsx
+++ site/src/components/pages/LandingPage/LandingPage.tsx
@@ -4,16 +4,11 @@ import { Intro } from './Intro/Intro'
 import { Roadmap } from './Roadmap/Roadmap'
 import { Sponsors } from './Sponsors/Sponsors'
 import { Line } from './Line'
-import { Uniswap } from './Uniswap/Uniswap'
-import ClientOnly from './Uniswap/ClientOnly'
 
 export const LandingPage = () => {
   return (
     <div>
       <Intro />
-      <ClientOnly>
-        <Uniswap />
-      </ClientOnly>
       <Community />
       <Roadmap />
       <Line />
